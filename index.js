3b1602d2c88d076f51b4947b6428128898705993
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-03-28 03:05:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var kittens = ['Milo', 'Otis', 'Garfield']; //define your array here
 
 // Add your functions and code here
-var destructivelyAppendKitten = function( array, name){
+function destructivelyAppendKitten( array, name){
   array.unshift(name);
   return array;
 }
