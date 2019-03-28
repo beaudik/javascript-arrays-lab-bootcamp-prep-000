4f7c9e696dfef756091cb2ab4f18494891c9e95b
Dikla Beauliere <dikla.beauliere@gmail.com>
2019-03-28 03:45:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -27,5 +27,5 @@ function prependKitten(name){
  return [name, ...kittens];
 }
 function removeLastKitten(){
- return kittens.slice(kittens.length-1);
+ return kittens.slice(0, kittens.length - 1);
 }
